4daf1cda16cf876e09cfc2c6f2ea6c079fcf0009
taylor herbots <therbots@live.com>
2021-09-15 14:41:30 -0500

    edited video class and registered it in admin.py

--- you_tube_clone/videos/models.py
+++ you_tube_clone/videos/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
 class Video(models.Model):
+    video_id = models.CharField(max_length=50)
     like = models.IntegerField()
     dislike = models.IntegerField()
     comment = models.CharField(max_length=150)
+    reply = models.CharField(max_length=150)
